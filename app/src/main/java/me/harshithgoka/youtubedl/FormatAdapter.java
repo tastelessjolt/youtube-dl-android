1b8ca26ce552debe74a2ea2f4d37a450ddace74d
Harshith Goka <harshith9399@gmail.com>
2018-08-25 01:11:45 +0530

    migrate to androidx

--- app/src/main/java/me/harshithgoka/youtubedl/FormatAdapter.java
+++ app/src/main/java/me/harshithgoka/youtubedl/FormatAdapter.java
@@ -4,7 +4,7 @@ import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
 import android.net.Uri;
-import android.support.v7.widget.RecyclerView;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
--- app/src/main/java/me/harshithgoka/youtubedl/FormatsActivity.java
+++ app/src/main/java/me/harshithgoka/youtubedl/FormatsActivity.java
@@ -3,12 +3,12 @@ package me.harshithgoka.youtubedl;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Parcelable;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 
 import java.util.List;
